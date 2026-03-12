71c6d31ed6cf7d33948a95385f2830698a04f9cd
srenegado <scottstationary@gmail.com>
2026-03-12 13:52:32 -0700

    Add headline and basic styling

--- src/load_page.js
+++ src/load_page.js
@@ -1,5 +1,14 @@
 import "./style.css";
 
 export function loadPage() {
-  
+  const content = document.querySelector("#content");
+  const headline = document.createElement("span");
+  const headlineWrapper = document.createElement("div");
+
+  headline.textContent = "Ridge Cafe";
+  headline.classList.add("headline");
+  headlineWrapper.classList.add("headline-wrapper");
+
+  headlineWrapper.appendChild(headline);
+  content.appendChild(headlineWrapper);
 }
