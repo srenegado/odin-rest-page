5c59073d8288036be87db94738f96c4eed48fe03
srenegado <scottstationary@gmail.com>
2026-03-16 14:58:12 -0700

    Finish up basic loadPage logic

--- src/load_page.js
+++ src/load_page.js
@@ -4,11 +4,22 @@ export function loadPage() {
   const content = document.querySelector("#content");
   const headline = document.createElement("span");
   const headlineWrapper = document.createElement("div");
+  const blurb = document.createElement("p");
+  const blurbHeading = document.createElement("h2");
+  const blurbWrapper = document.createElement("div");
 
   headline.textContent = "Ridge Cafe";
   headline.classList.add("headline");
   headlineWrapper.classList.add("headline-wrapper");
-
   headlineWrapper.appendChild(headline);
+
+  blurb.textContent = "A short little blurb tagline for the restaurant--what we serve, what we're known for, etc.";
+  blurb.classList.add("blurb");
+  blurbHeading.textContent = "Blurb heading";
+  blurb.insertBefore(blurbHeading, blurb.firstChild);
+  blurbWrapper.classList.add("blurb-wrapper");
+  blurbWrapper.appendChild(blurb);
+
   content.appendChild(headlineWrapper);
+  content.appendChild(blurbWrapper);
 }
